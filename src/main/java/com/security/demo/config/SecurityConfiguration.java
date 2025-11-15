1d36e48b7398ac16573271e69e8237e57d8ee315
Serhii Shapoval <acidserg359@gmail.com>
2025-11-15 17:07:09 +0200

    Added: In-memory user/admin creation; path-based security; controller examples with different security access, service class with method level security; debug log level for spring security package

--- src/main/java/com/security/demo/config/SecurityConfiguration.java
+++ src/main/java/com/security/demo/config/SecurityConfiguration.java
@@ -4,21 +4,65 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // SpringBootWebSecurityConfiguration.java
 @Configuration
+@EnableWebSecurity
+/*
+1. prePostEnabled = true: Enables Spring Security's @PreAuthorize and @PostAuthorize annotations
+ for method-level security checks before and after method execution.
+
+2.securedEnabled = true: Enables support for the @Secured annotation,
+ allowing role-based access control on methods.
+
+3. jsr250Enabled = true: Enables support for JSR-250 annotations like @RolesAllowed,
+ providing standard Java role-based security on methods.
+ */
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+    http.authorizeHttpRequests(
+        (requests) ->
+            requests
+                .requestMatchers("/private/**")
+                .authenticated()
+                .requestMatchers("/public/**")
+                .permitAll()
+                .requestMatchers("/restricted/**")
+                .denyAll()
+                .anyRequest()
+                .authenticated());
     http.sessionManagement(
         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     http.httpBasic(withDefaults());
-    //http.formLogin(withDefaults());
     return http.build();
+  }
+
+  /*
+  {noop} is a password encoder prefix in Spring Security that means
+   "no operation"—the password is stored and compared as plain text without any encoding or hashing.
+   */
+  @Bean
+  public UserDetailsService userDetailsService() {
+    final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    if (!manager.userExists("user")) {
+      manager.createUser(
+          User.withUsername("user").password("{noop}userpassword").roles("USER").build());
+    }
+    if (!manager.userExists("admin")) {
+      manager.createUser(
+          User.withUsername("admin").password("{noop}adminpassword").roles("ADMIN").build());
+    }
+    return manager;
   }
 }
--- src/main/java/com/security/demo/controller/SpringSecurityTestController.java
+++ src/main/java/com/security/demo/controller/SpringSecurityTestController.java
@@ -1,12 +1,57 @@
 package com.security.demo.controller;
 
+import com.security.demo.service.UserDetailsServiceSecure;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringSecurityTestController {
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, Spring Security!";
+
+  private final UserDetailsServiceSecure userDetailsServiceSecure;
+
+  public SpringSecurityTestController(UserDetailsServiceSecure userDetailsServiceSecure) {
+    this.userDetailsServiceSecure = userDetailsServiceSecure;
+  }
+
+  @GetMapping("/restricted/information")
+  public String restrictedInformation() {
+    return "Restricted information endpoint";
+  }
+
+  @PreAuthorize("hasRole('USER')")
+  @GetMapping("/user/details")
+  public String getUserDetails() {
+    return "User details endpoint";
+  }
+
+  @GetMapping("/user/details/method")
+  public String getUserDetailsMethodLevelSecurity() {
+    return this.userDetailsServiceSecure.getUserDetailsMethodLevelSecurity();
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @GetMapping("/admin/details")
+  public String getAdminDetails() {
+    return "Admin details endpoint";
+  }
+
+  @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+  //@PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN')")
+  @GetMapping("/user-admin/details")
+  public String getUserAndAdminDetails(@AuthenticationPrincipal final UserDetails userDetails) {
+    return "User-Admin details endpoint. Current User: " + userDetails.getUsername();
+  }
+
+  @GetMapping("/private/information")
+  public String privateInformation() {
+    return "Private information endpoint";
+  }
+
+  @GetMapping("/public/information")
+  public String publicInformation() {
+    return "Public information endpoint";
   }
 }
